899ba5da7613f41f6d5fa27c9279cd42fe562720
ncardinale <ndc309@nyu.edu>
2019-11-14 12:49:51 -0500

    New Changes

--- Homework/script.js
+++ Homework/script.js
@@ -2,13 +2,17 @@
 var SingleNoun
 var PluralNoun
 var Adjective
-var MLbutton = $("MLbutton").on("click", PutWords)
+var MLbutton = $("MLbutton").on("click", function PutWords)
 
+function PutWords(){
+  var noun1 = $('#SingleNoun').val();
+  $('#word1').text(noun1);
+}
 
-$('#MLbutton').click(function PutWords() {
-  var noun1 = $('#word1').val();
-  $('#SingleNoun').text(noun1);
-});
+//$('#MLbutton').click(function PutWords() {
+//  var noun1 = $('#word1').val();
+//$('#SingleNoun').text(noun1);
+//});
 
 //var input = "";
 //function() = input;
